7a80909dfe5b00b32d82e2432dbbb77aea104fd2
Vignesh Subramanian <vignesh.sr.1991@gmail.com>
2024-08-13 08:03:51 +0530

    dsa-day-1

---
+++ min-time-to-visit-all-points.py
@@ -0,0 +1,16 @@
+class MinTimeVisitingAllPts:
+    def solve(self,points):
+        answer = 0
+        currentPosition = points[0]
+        for i in range(1,len(points)):
+            targetPosition = points[i]
+            # difference in position of x
+            dx = targetPosition[0] - currentPosition[0]
+            dy = targetPosition[1] - currentPosition[1]
+            answer += max(abs(dx), abs(dy))
+            currentPosition = targetPosition
+        return answer
+if __name__ == '__main__':
+    points = [[1,1],[3,4],[-1,0]]
+    result = MinTimeVisitingAllPts().solve(points)
+    print(result)
